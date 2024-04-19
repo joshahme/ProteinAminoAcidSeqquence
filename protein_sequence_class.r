aff67b00660b26fd05ac9d99102bfedc1519e9ee
joshahme <155593968+joshahme@users.noreply.github.com>
2024-04-18 21:55:37 -0400

    Delete protein_sequence_class.r

--- protein_sequence_class.r
+++
@@ -1,8 +0,0 @@
-# Define the S3 class for protein sequences
-setClass("protein_seq",
-         slots = list(sequence = "character"))
-
-# Constructor function for protein_seq class
-protein_seq <- function(sequence) {
-  new("protein_seq", sequence = sequence)
-}
