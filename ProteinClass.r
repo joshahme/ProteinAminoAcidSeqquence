b418dedfe240984cfba5c133dce911f328a0fd33
joshahme <155593968+joshahme@users.noreply.github.com>
2024-04-18 21:53:51 -0400

    Delete ProteinClass.r

--- ProteinClass.r
+++
@@ -1,8 +0,0 @@
-# Define the S4 class for protein objects
-setClass("Protein",
-         slots = list(sequence = "character"))
-
-# Constructor function for Protein class
-Protein <- function(sequence) {
-  new("Protein", sequence = sequence)
-}
